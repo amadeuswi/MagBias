5a2ed9b688b85d79a4361b1bf803df4c2a92ec50
Amadeus <amsl@gmx.at>
2018-05-16 12:00:17 +0100

    trying again

--- magbias_experiments.py
+++ magbias_experiments.py
@@ -28,3 +28,27 @@ CLAR = {
     "S_area": A_CLAR,
     "Name" : "CLAR"
 }
+
+#
+# hirax = {
+#     'mode':             'interferom',            # Interferometer or single dish
+#     'Ndish':            1024,                 # No. of dishes
+#     'Nbeam':            1,                # No. of beams (for multi-pixel detectors)
+#     'Ddish':            6.,               # Single dish diameter [m]
+#     'Tinst':            50.*(1e3),         # System temp. [mK]
+#     #'nu_crit':          1000.,            #critical frequency, UNCLEAR
+#     'survey_dnutot':    400.,              # Total bandwidth of *entire* survey [MHz]
+#     'survey_numax':     800.,             # Max. freq. of survey
+#     'dnu':              0.4,               # Bandwidth of single channel [MHz]
+#     'pointings':        1,                  # number of pointings in drift scan
+#     'obs_per_day':      12.,                 #observation hours per day
+#     # 'Sarea':            2*np.pi, #half sky    # Total survey area [radians^2]
+#     'Dmax':             307.,               # Max. interferom. baseline [m] UNCLEAR
+#     'Dmin':             7.,                 # Min. interferom. baseline [m] UNCLEAR
+#     'wiggleroom':       1.,                  #wiggle room between dishes
+#     'k_nl0':            0.2             # inv Mpc, nonlinear k cutoff
+# }
+# hirax.update(SURVEY)
+# cb_hirax=dict(hirax)
+#
+# cb_hirax.update({'n(x)':             "/Users/amadeus/Documents/PhD/work/bao21cm/hirax/hirax_Ndish1024_baseline1.dat"})
